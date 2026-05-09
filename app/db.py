b13aeec8c305663058ecd6e38565e9938bd35997
Joselito17821 <josemanuelmany04@gmail.com>
2026-05-09 02:31:58 -0500

    feat: Add Habit model with SQLAlchemy ORM

--- app/db.py
+++ app/db.py
@@ -6,3 +6,5 @@ from app.core.config import DATABASE_URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+from app.models.habit import Habit
